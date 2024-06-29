a20606e8289cff6758abfb04beb83fb12cf47c13
Cristobal-coding <agartkg95@gmail.com>
2024-06-29 18:26:24 -0400

    Adding simple carusel bootstrap 5.3

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,11 +1,11 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { MatButton } from '@angular/material/button';
 
 @Component({
   standalone: true,
-  imports: [],
+  imports: [CommonModule, MatButton],
   templateUrl: './home.component.html',
-  styleUrl: './home.component.scss'
+  styleUrl: './home.component.scss',
 })
-export class HomeComponent {
-
-}
+export class HomeComponent {}
